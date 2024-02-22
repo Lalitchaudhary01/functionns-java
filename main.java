35cea32433ab356a35211cec2cb7924adb69865a
Lalit Chaudhary <120069324+Lalitchaudhary01@users.noreply.github.com>
2024-02-22 21:44:20 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        // Input
+        System.out.println("my name is Lalit");
+        System.out.println("11082004");
+
+    }
+}
